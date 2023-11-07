fc3ca27bd5cf5fc6d989d544aa50a2a2ee487dfa
IDKWNTCMF <71064019+IDKWNTCMF@users.noreply.github.com>
2023-11-07 16:42:29 +0300

    Add DB for tilesets. Extend server API. Code refactoring. (#32)

---
+++ src/main/kotlin/db/DBTilesetSchema.kt
@@ -0,0 +1,26 @@
+package db
+
+import org.jetbrains.exposed.dao.*
+import org.jetbrains.exposed.dao.id.*
+
+object TilesetTable: IntIdTable("tileset", "tileset_id") {
+    val pathToJson = varchar("path_to_json", pathLength).uniqueIndex()
+}
+
+class TilesetData(id: EntityID<Int>): IntEntity(id) {
+    companion object: IntEntityClass<TilesetData>(TilesetTable)
+
+    var pathToJson by TilesetTable.pathToJson
+
+    fun raw(): TilesetInfo = TilesetInfo(id.value.toUInt(), pathToJson)
+}
+
+data class TilesetInfo(val id: UInt, val pathToJson: String) {
+    fun load(): Tileset = Tileset(pathToJson)
+}
+
+class Tileset(pathToJson: String) {
+    init {
+        TODO()
+    }
+}
