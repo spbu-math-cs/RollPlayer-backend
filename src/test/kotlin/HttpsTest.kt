bb22d007867662ab42c7b63a76d0925aa2481310
Maria Loskutova <110923631+minstradamuss@users.noreply.github.com>
2023-12-21 23:33:56 +0300

    Fix:2?

--- src/test/kotlin/HttpsTest.kt
+++ src/test/kotlin/HttpsTest.kt
@@ -1,27 +1,101 @@
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
 import com.typesafe.config.ConfigFactory
+import db.DBOperator
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
+import io.ktor.serialization.kotlinx.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
 import io.ktor.server.config.*
 import io.ktor.server.engine.*
+import io.ktor.server.http.content.*
 import io.ktor.server.netty.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 import io.ktor.server.testing.*
+import io.ktor.server.websocket.*
 import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.json.Json
 import org.json.JSONObject
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
+import server.ActiveSessionData
 import server.JWTParams
-import server.extracted
+
+import server.logger
+import server.routing.*
 import java.io.File
+import java.time.Duration
+import java.util.*
 
 private fun createErrorResponseMessage(msg: String?) = mapOf(
     "type" to "error", "message" to msg
 ).toString()
+
+fun Application.extractedT(jwtParams: JWTParams) {
+    install(io.ktor.server.plugins.cors.routing.CORS) {
+        anyHost()
+    }
+    install(ContentNegotiation) {
+        json()
+    }
+    install(WebSockets) {
+        contentConverter = KotlinxWebsocketSerializationConverter(Json)
+        pingPeriod = Duration.ofSeconds(2)
+        maxFrameSize = Long.MAX_VALUE
+        masking = false
+    }
+    install(Authentication) {
+        jwt ("auth-jwt") {
+            realm = jwtParams.myRealm
+            verifier(
+                JWT
+                    .require(Algorithm.HMAC256(jwtParams.secret))
+                    .withAudience(jwtParams.audience)
+                    .withIssuer(jwtParams.issuer)
+                    .build())
+            validate { credential ->
+                if (credential.payload.getClaim("id").asString() != "" &&
+                    credential.payload.getClaim("login").asString() != "" &&
+                    (credential.expiresAt?.time?.minus(System.currentTimeMillis()) ?: 0) > 0
+                ) {
+                    JWTPrincipal(credential.payload)
+                } else {
+                    null
+                }
+            }
+            challenge { _, _ ->
+                call.respond(HttpStatusCode.Unauthorized, "Token is not valid or has expired")
+            }
+        }
+    }
+
+    val activeSessions = Collections.synchronizedMap<UInt, ActiveSessionData>(mutableMapOf())
+
+    DBOperator.connectOrCreate(true)
+
+    logger.info("Server is ready")
+    routing {
+        staticFiles("", File("static"))
+
+        requestsUser(jwtParams)
+        requestsMap()
+        requestsPictures()
+
+        gameSession()
+        gameSessionConnection(activeSessions)
+    }
+}
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class HttpsTest {
@@ -33,7 +107,7 @@ class HttpsTest {
     val myRealm = config.property("jwt.realm").getString()
     init {
         server = embeddedServer(Netty, port = 1234) {
-            extracted(JWTParams(secret, issuer, audience, myRealm))
+            extractedT(JWTParams(secret, issuer, audience, myRealm))
 
         }
         server.start(wait = false)
